e66a18c99470b4c1c1d1e50a26a464475e2361a4
krusand <110490348+krusand@users.noreply.github.com>
2023-05-04 13:53:29 +0200

    Added otsu threshold
    Also added docstrings to functions

--- Segmentation.py
+++ Segmentation.py
@@ -4,12 +4,24 @@ import matplotlib.pyplot as plt
 from skimage.color import rgb2gray
 from skimage.filters import gaussian
 from skimage.segmentation import active_contour
-from skimage.filters import threshold_otsu
 from skimage.draw import polygon
 from skimage.transform import resize
-
+from skimage.filters import threshold_otsu
 
 def loadImg(path, resize_image = False, color=False):
+    """
+    Loads image given a path.
+        Parameters: 
+            path : str 
+                Path to the image
+            resize_image : bool
+                Option to resize image or not
+            color : bool
+                Load image with or without colors
+
+        Returns: 
+            np.array containing pixel values
+    """
     if resize_image:
         im = rgb2gray(plt.imread(path)[:,:,:3])
         return resize(im, (im.shape[0] // 4, im.shape[1] // 4), anti_aliasing=True)
@@ -20,9 +32,11 @@ def loadImg(path, resize_image = False, color=False):
 
 
 def blurImg(im,strength):
+    """Blurs the image by a given strength and returns it"""
     return gaussian(im, strength,preserve_range=False)
 
 def activeContourMask(im, radius, xval, yval):
+    """Creates an active contour mask"""
     s = np.linspace(0, 2*np.pi, 200)
     r = yval + radius*np.sin(s)
     c = xval + radius*np.cos(s)
@@ -34,17 +48,25 @@ def activeContourMask(im, radius, xval, yval):
     return mask
 
 def plotImg(im):
+    """Plots a grayscale image"""
     plt.imshow(im, cmap="gray")
     plt.show()
 
-
-
 def plotImageWithMask(im, mask):
+    """Plots a binary mask on top of an grayscale image"""
     im2 = im.copy()
     im2[mask == 0] = 0
     plt.imshow(im2, cmap="gray")
     plt.show()
 
+def otsuThreshold(img):
+    """
+    Computes the otsu threshold and returns a binary mask
+    where all pixels less than the threshold are white and otherwise black
 
-
+    Input: Grayscale image
+    Outputs: Binary mask
+    """
+    threshold = threshold_otsu(img)
+    return img < threshold
 
