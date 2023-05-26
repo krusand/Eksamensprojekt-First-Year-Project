9cd0244cc8701e32e5754bb30a1a233177d9dc1f
krusand <110490348+krusand@users.noreply.github.com>
2023-05-26 15:55:26 +0200

    Added comparison for fitspatrick

---
+++ FitzpatrickComparison.py
@@ -0,0 +1,36 @@
+import derm_ita
+import os
+from tqdm import tqdm
+from PIL import Image
+import pandas as pd
+import random
+
+path = "Data/images"
+images = os.listdir(path)
+
+# Testing:
+# scores = {img_id: random.randint(1, 6) for img_id in img_ids}
+
+scores = {}
+
+for image in tqdm(images):
+    whole_image_ita = derm_ita.get_ita(image=Image.open(path + "/" + image))
+    scores[image] = derm_ita.get_fitzpatrick_type(whole_image_ita)
+
+raw_df = pd.read_csv("data/metadata.csv")
+df = raw_df.loc[:, ["img_id", "fitspatrick"]]
+df = df.dropna()
+
+img_ids = df["img_id"].values
+fitspatrick_metadata = df["fitspatrick"].values
+
+metadata_scores = {img_ids[i]: fitspatrick_metadata[i] for i in range(len(img_ids))}
+
+
+count = 0
+
+for key in scores:
+    if metadata_scores[key] == scores[key]:
+        count += 1
+
+print(count / len(scores))
