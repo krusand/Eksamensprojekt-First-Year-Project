109e54bd00b08f37d23c3d35d2982c32020cae6f
swae <swae@itu.dk>
2023-05-29 15:33:16 +0200

    Correction of multicolor rate feature extraction

--- _1features.py
+++ _1features.py
@@ -67,27 +67,21 @@ class FeatureReader:
         pixel_color = np.array([red_avg, green_avg, blue_avg])
         return pixel_color
 
+
     def get_com_col(sef, cluster, centroids, com_col_list):
+        com_col_list = []
         labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
         (hist, _) = np.histogram(cluster.labels_, bins=labels)
         hist = hist.astype("float")
         hist /= hist.sum()
 
-        rect = np.zeros((50, 300, 3), dtype=np.uint8)
-        colors = sorted([(percent, color) for (percent, color) in zip(hist, centroids)])
-        start = 0
+        colors = [(percent, color) for (percent, color) in zip(hist, centroids)]
         for percent, color in colors:
             if percent > 0.08:
                 com_col_list.append(color)
-            end = start + (percent * 300)
-            cv2.rectangle(
-                rect,
-                (int(start), 0),
-                (int(end), 50),
-                color.astype("uint8").tolist(),
-                -1,
-            )
-            start = end
+                
+        return com_col_list
+            
 
     def get_multicolor_rate(self, im, mask, n):
         im = resize(im, (im.shape[0] // 4, im.shape[1] // 4), anti_aliasing=True)
@@ -105,10 +99,8 @@ class FeatureReader:
                 if mask[i][j] != 0:
                     col_list.append(im2[i][j] * 256)
 
-        com_col_list = []
-
         cluster = KMeans(n_clusters=n).fit(col_list)
-        self.get_com_col(cluster, cluster.cluster_centers_, com_col_list)
+        com_col_list = self.get_com_col(cluster, cluster.cluster_centers_)
 
         dist_list = []
         m = len(com_col_list)
