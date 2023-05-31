485b56ef202a3d1cbde433523c67cd78cb4d1ba2
Joke Heinks <bhei@itu.dk>
2023-05-31 16:54:06 +0200

    Fixed features.py Calculated features for all images

--- features.py
+++ features.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 from skimage.transform import resize
 from skimage.transform import rotate
@@ -14,144 +15,174 @@ import cv2
 from sklearn.cluster import KMeans
 
 
-class FeatureReader:
-    def extractFeatures(self, mask_path, img_path, metadata):
-        image_names = sorted(glob(f"{img_path}/*.png"))
-        mask_names = sorted(glob(f"{mask_path}/*.png"))
-        # df = self.__readMetadata(metadata)
-        df = pd.DataFrame()
+def extractFeatures(mask_path, img_path, metadata):
+    names = [s.split(os.sep)[-1] for s in sorted(glob(f"{mask_path}/*.png"))]
+    # df = self.__readMetadata(metadata)
+    df = pd.DataFrame()
 
-        compactness = []
-        avg_red_channel = []
-        avg_green_channel = []
-        avg_blue_channel = []
-        multicolor_rate = []
+    image_names = []
+    compactness = []
+    avg_red_channel = []
+    avg_green_channel = []
+    avg_blue_channel = []
+    multicolor_rate = []
+    asymmetry = []
+    
+    for name in tqdm(names[:50]):
+        mask = plt.imread(os.path.join(mask_path, name))
+        image = plt.imread(os.path.join(img_path, name))[:, :, :3]
 
-        for img, mask in tqdm(zip(image_names, mask_names)):
-            mask = plt.imread(mask)
-            image = plt.imread(img)[:, :, :3]
-            mask = resize(mask, output_shape=image.shape)
-            mask = color.rgb2gray(mask)
+        mask = binary_mask(mask, image.shape)
+        if np.sum(mask) == 0:
+             print(f"Empty mask for image {name}")
+             continue
 
-            r, g, b = self.__averageColor(image, mask)
-            avg_red_channel.append(r)
-            avg_green_channel.append(g)
-            avg_blue_channel.append(b)
-            compactness.append(self.__compactness(mask))
-            multicolor_rate.append(self.get_multicolor_rate(image, mask, 3))
 
-        df["compactness"] = compactness
-        df["avg_red_channel"] = avg_red_channel
-        df["avg_green_channel"] = avg_green_channel
-        df["avg_blue_channel"] = avg_blue_channel
-        df["multicolor_rate"] = multicolor_rate
+        image_names.append(name)
 
-        return df
+        r, g, b = averageColor(image, mask)
+        avg_red_channel.append(r)
+        avg_green_channel.append(g)
+        avg_blue_channel.append(b)
+        compactness.append(get_compactness(mask))
+        multicolor_rate.append(get_multicolor_rate(image, mask, 3))
+        asymmetry.append(get_asymmetry(mask))
 
-    def __readMetadata(self, path):
-        return pd.read_csv(path)
+    df["image_names"] = image_names
+    df["compactness"] = compactness
+    df["avg_red_channel"] = avg_red_channel
+    df["avg_green_channel"] = avg_green_channel
+    df["avg_blue_channel"] = avg_blue_channel
+    df["multicolor_rate"] = multicolor_rate
+    df["asymmetry"] = asymmetry
 
-    def __compactness(self, mask):
-        # mask = color.rgb2gray(mask)
-        area = np.sum(mask)
+    return df
 
-        struct_el = morphology.disk(3)
-        mask_eroded = morphology.binary_erosion(mask, struct_el)
-        perimeter = np.sum(mask - mask_eroded)
+def binary_mask(mask, shape, threshold = 0.5):
+        mask = resize(mask, output_shape= shape)
+        mask = color.rgb2gray(mask)
+        mask[mask < threshold] = 0
+        mask[mask > threshold] = 1
+        return mask
 
-        return perimeter**2 / (4 * np.pi * area)
+def readMetadata(self, path):
+    return pd.read_csv(path)
 
-    def __averageColor(self, img, mask):
-        img[mask == 0] = 0
-        tot_pixels = np.sum(mask)
-        red_avg = np.sum(img[:, :, 0]) / tot_pixels
-        green_avg = np.sum(img[:, :, 1]) / tot_pixels
-        blue_avg = np.sum(img[:, :, 2]) / tot_pixels
-        pixel_color = np.array([red_avg, green_avg, blue_avg])
-        return pixel_color
+def get_compactness(mask):
+    # mask = color.rgb2gray(mask)
+    area = np.sum(mask)
 
-    def get_com_col(sef, cluster, centroids):
-        com_col_list = []
-        labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
-        (hist, _) = np.histogram(cluster.labels_, bins=labels)
-        hist = hist.astype("float")
-        hist /= hist.sum()
+    struct_el = morphology.disk(3)
+    mask_eroded = morphology.binary_erosion(mask, struct_el)
+    perimeter = np.sum(mask - mask_eroded)
 
-        rect = np.zeros((50, 300, 3), dtype=np.uint8)
-        colors = sorted([(percent, color) for (percent, color) in zip(hist, centroids)], key= lambda x:x[0])
-        start = 0
-        for percent, color in colors:
-            if percent > 0.08:
-                com_col_list.append(color)
-            end = start + (percent * 300)
-            cv2.rectangle(
-                rect,
-                (int(start), 0),
-                (int(end), 50),
-                color.astype("uint8").tolist(),
-                -1,
-            )
-            start = end
-        return com_col_list
+    return perimeter**2 / (4 * np.pi * area)
 
-    def get_multicolor_rate(self, im, mask, n):
-        # mask = color.rgb2gray(mask)
-        im = resize(im, (im.shape[0] // 4, im.shape[1] // 4), anti_aliasing=True)
-        mask = resize(
-            mask, (mask.shape[0] // 4, mask.shape[1] // 4), anti_aliasing=True
+def averageColor(img, mask):
+    img[mask == 0] = 0
+    tot_pixels = np.sum(mask)
+    red_avg = np.sum(img[:, :, 0]) / tot_pixels
+    green_avg = np.sum(img[:, :, 1]) / tot_pixels
+    blue_avg = np.sum(img[:, :, 2]) / tot_pixels
+    pixel_color = np.array([red_avg, green_avg, blue_avg])
+    return pixel_color
+
+def get_com_col(cluster, centroids):
+    com_col_list = []
+    labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
+    (hist, _) = np.histogram(cluster.labels_, bins=labels)
+    hist = hist.astype("float")
+    hist /= hist.sum()
+
+    rect = np.zeros((50, 300, 3), dtype=np.uint8)
+    colors = sorted([(percent, color) for (percent, color) in zip(hist, centroids)], key= lambda x:x[0])
+    start = 0
+    for percent, color in colors:
+        if percent > 0.08:
+            com_col_list.append(color)
+        end = start + (percent * 300)
+        cv2.rectangle(
+            rect,
+            (int(start), 0),
+            (int(end), 50),
+            color.astype("uint8").tolist(),
+            -1,
         )
-        im2 = im.copy()
-        im2[mask == 0] = 0
+        start = end
+    return com_col_list
 
-        columns = im.shape[0]
-        rows = im.shape[1]
-        col_list = []
-        for i in range(columns):
-            for j in range(rows):
-                if mask[i][j] != 0:
-                    col_list.append(im2[i][j] * 256)
+def get_multicolor_rate(im, mask, n):
+    # mask = color.rgb2gray(mask)
+    im = resize(im, (im.shape[0] // 4, im.shape[1] // 4), anti_aliasing=True)
+    mask = resize(
+        mask, (mask.shape[0] // 4, mask.shape[1] // 4), anti_aliasing=True
+    )
+    im2 = im.copy()
+    im2[mask == 0] = 0
 
-        if len(col_list) == 0:
-            return ""
+    columns = im.shape[0]
+    rows = im.shape[1]
+    col_list = []
+    for i in range(columns):
+        for j in range(rows):
+            if mask[i][j] != 0:
+                col_list.append(im2[i][j] * 256)
 
-        cluster = KMeans(n_clusters=n, n_init=10).fit(col_list)
-        com_col_list = self.get_com_col(cluster, cluster.cluster_centers_)
+    if len(col_list) == 0:
+        return ""
 
-        dist_list = []
-        m = len(com_col_list)
+    cluster = KMeans(n_clusters=n, n_init=10).fit(col_list)
+    com_col_list = get_com_col(cluster, cluster.cluster_centers_)
 
-        if m <= 1:
-            return ""
+    dist_list = []
+    m = len(com_col_list)
 
-        for i in range(0, m - 1):
-            j = i + 1
-            col_1 = com_col_list[i]
-            col_2 = com_col_list[j]
-            dist_list.append(
-                np.sqrt(
-                    (col_1[0] - col_2[0]) ** 2
-                    + (col_1[1] - col_2[1]) ** 2
-                    + (col_1[2] - col_2[2]) ** 2
-                )
+    if m <= 1:
+        return ""
+
+    for i in range(0, m - 1):
+        j = i + 1
+        col_1 = com_col_list[i]
+        col_2 = com_col_list[j]
+        dist_list.append(
+            np.sqrt(
+                (col_1[0] - col_2[0]) ** 2
+                + (col_1[1] - col_2[1]) ** 2
+                + (col_1[2] - col_2[2]) ** 2
             )
-        return np.max(dist_list)
+        )
+    return np.max(dist_list)
 
-    def __asymetry(self, mask):
-        # mask = color.rgb2gray(mask)
-        scores = []
-        for _ in range(6):
-            segment = self.__crop(mask)
-            (np.sum(segment))
-            scores.append(np.sum(np.logical_xor(segment, np.flip(segment))) / (np.sum(segment)))
-            mask = rotate(mask, 30)
-        return sum(scores) / len(scores)
+def midpoint(mask):
+        summed = np.sum(mask, axis=0)
+        half_sum = np.sum(summed) / 2
+        for i, n in enumerate(np.add.accumulate(summed)):
+            if n > half_sum:
+                return i
+def crop(mask):
+        mid = midpoint(mask)
+        y_nonzero, x_nonzero = np.nonzero(mask)
+        y_lims = [np.min(y_nonzero), np.max(y_nonzero)]
+        x_lims = np.array([np.min(x_nonzero), np.max(x_nonzero)])
+        x_dist = max(np.abs(x_lims - mid))
+        x_lims = [mid - x_dist, mid+x_dist]
+        return mask[y_lims[0]:y_lims[1], x_lims[0]:x_lims[1]]
+
+def get_asymmetry(mask):
+    # mask = color.rgb2gray(mask)
+    scores = []
+    for _ in range(6):
+        segment = crop(mask)
+        (np.sum(segment))
+        scores.append(np.sum(np.logical_xor(segment, np.flip(segment))) / (np.sum(segment)))
+        mask = rotate(mask, 30)
+    return sum(scores) / len(scores)
 
 def main():
-    FR = FeatureReader()
-    df = FR.extractFeatures(
+    df = extractFeatures(
         mask_path="Data/masks", img_path="Data/images", metadata="metadata.csv"
     )
-    df.to_csv("Data/features.csv")
+    df.to_csv("Data/features.csv", index= False)
 
 
 if __name__ == "__main__":
