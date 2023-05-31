ed503433590f361f0e5ff3f919225dcddecb99f2
Joke Heinks <bhei@itu.dk>
2023-05-31 18:05:53 +0200

    Changed classifiers to merge the dataframes Printing the Scores

--- _2train_classifiers.py
+++ _2train_classifiers.py
@@ -4,16 +4,29 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, roc_auc_score
 
+SEED = 42
+METADATA_PATH = "Data/metadata.csv"
+FEATURES_PATH = "Data/features.csv"
 
 # loading and preparing data
-raw_df = pd.read_csv("metadata.csv")
-features = pd.read_csv("features.csv")
+metadata = pd.read_csv(METADATA_PATH)
+features = pd.read_csv(FEATURES_PATH)
+
+def train_test_val_split(X, y, train_size = 0.7, test_size = 0.2, val_size = 0.1):
+    x_train, x_val, y_train, y_val = train_test_split(X,y, train_size= 1-val_size, shuffle=True, random_state = SEED)
+    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, train_size = train_size / (1-val_size))
+    return x_train, x_test, x_val, y_train, y_test, y_val
+
+df = pd.merge(metadata, features, left_on= "img_id", right_on= "image_names")
+
+
 COI = ["diagnostic", "age", "itch", "grew", "hurt", "changed", "bleed", "elevation", "biopsed"]
 COI2 = ["diagnostic", "age", "itch", "grew", "hurt", "changed", "bleed", "elevation", "biopsed", "fitspatrick"]
-df = raw_df.loc[:,COI]
-df2 = raw_df.loc[:,COI2]
-df = df.dropna()
+df1 = df.loc[:,COI]
+df2 = df.loc[:,COI2]
+df = df1.dropna()
 df2 = df2.dropna()
 COIbool = ["itch", "grew", "hurt", "changed", "bleed", "elevation", "biopsed"]
 d = {"False": 0, "True": 1, False: 0, True:1, "UNK": 0.5}
@@ -29,13 +42,38 @@ y = y.replace(cancers)
 
 
 scalar = StandardScaler()
-X_norm = scalar.fit_transform(X)
-X_train, X_test, y_train, y_test = train_test_split(X_norm, y,test_size=0.25, train_size=0.75)
+X = scalar.fit_transform(X)
+
+X_train, X_test, X_val, y_train, y_test, y_val = train_test_val_split(X,y)
+
 
 # KNN-classifiers
 knnc = KNeighborsClassifier(n_neighbors=20)
 
+n_vals = [i for i in range(1,164)]
+n_vals = [i for i in range(1,int(len(X_train) ** 0.5))]
+n_vals = [i for i in range(1,500)]
+knn_classifiers = []
+
+for i in n_vals:
+    knnc = KNeighborsClassifier(n_neighbors=i)
+    knnc.fit(X_train, y_train)
+    knn_classifiers.append(knnc)
+
+predicted_ys = [model.predict(X_test) for model in knn_classifiers]
+accuracy_scores = [accuracy_score(y_test, y_pred) for y_pred in predicted_ys]
+roc_auc_scores = [roc_auc_score(y_test,y_pred) for y_pred in predicted_ys]
+
+
+for i, val in enumerate(n_vals):
+   print(f"N = {val} \t Accuracy scores = {round(accuracy_scores[i], 4)} \t ROC AUC Scores = {round(roc_auc_scores[i],4)}")
+
+import matplotlib.pyplot as plt
+plt.scatter(n_vals, roc_auc_scores)
+plt.show()
 
 # logistic regression
 clf  = LogisticRegression().fit(X = X_train, y = y_train)
 
+pred = clf.predict(X_test)
+print(clf.score(X_test, y_test))
